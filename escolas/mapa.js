090e8fd579359f6599eb59c4e060401e708f7b21
Vanessa Cordeiro <vanessa.cordeiro148@gmail.com>
2016-09-14 10:18:34 -0300

    ...

--- escolas/mapa.js
+++ escolas/mapa.js
@@ -1,6 +1,8 @@
-
-	new GMaps({
-		  div: '#map',
-		  lat: -12.043333,
-		  lng: -77.028333
-		});
+$(document).ready(function(){
+      prettyPrint();
+      map = new GMaps({
+        div: '#map',
+        lat: -12.043333,
+        lng: -77.028333
+      });
+});
