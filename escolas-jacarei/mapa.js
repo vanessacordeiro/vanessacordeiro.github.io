852610ca2c553c9b53a7ea3734448cf616f7035d
Vanessa Cordeiro <vanessa.cordeiro148@gmail.com>
2016-09-21 08:39:28 -0300

    ...

--- escolas-jacarei/mapa.js
+++ escolas-jacarei/mapa.js
@@ -1,1 +1,19 @@
-document.write ("alô mundo !!!");
+$(document).ready(function(){
+			
+			$("#search").click(function() {
+			
+				
+				$.getJSON("https://api.myjson.com/bins/1ozje",function(data) {
+					
+					var values = [];
+					
+					$.each(data, function(i){
+						
+						values.push("<p>"+this.nome+"</p>");
+					});
+					
+					$("#data").append(values);
+					
+			});
+   });	
+});
